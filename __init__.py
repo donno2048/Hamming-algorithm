707135c385fca0e34c987741656e11a730c4fd83
Elisha Hollander <just4now666666@gmail.com>
2020-09-06 20:41:04 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,13 @@ def decode(x:list):
         if sum([sum(x[2**i+j::2**(i+1)]) for j in range(2**i)])%2==1:index+=2**i
     if sum(x)%2==1:x[index]=1-x[index]
     return x
-def decode2(x:list):index=reduce(lambda x,y:x^y,[i for i,logic in enumerate(x) if logic]);x[index]=1-x[index];return x #smaller version
+def decode2(x:list):index=reduce(lambda x,y:x^y,[i for i,logic in enumerate(x) if logic]);x[index]=1-x[index];return x #smaller version of decode
+def encode2(x:list): #smaller version of encode
+    for i in range(int.bit_length(len(x))-1):logic=(sum([sum(x[2**i+j::2**(i+1)]) for j in range(2**i)])%2==1);x[2**i]=(1-x[2**i])*logic+x[2**i]*(1-logic)
+    logic=sum(x)%2==1;x[0]=(1-x[0])*logic+x[0]*(1-logic);return x
+def ham2(x:list,PlaceHolder=0,new=[0],index=0): #smaller version of ham
+    for i in range(1,len(x)+int.bit_length(len(x))+1):logic=log2(i).is_integer();new+=[PlaceHolder*logic+x[index]*(1-logic)];index+=1-logic
+    return new
 PreHammedList=[1,0,1,0]
 print(ham(PreHammedList))
 InitialList=ham(PreHammedList)
@@ -24,4 +30,3 @@ print(encode(InitialList))
 CorruptedList=encode(InitialList)
 CorruptedList[4]=1-CorruptedList[4]
 print(decode(CorruptedList))
-print(decode2(CorruptedList))
