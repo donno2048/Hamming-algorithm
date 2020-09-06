4ffb8de8927550ffabd21088c02f34402192ba03
Elisha Hollander <just4now666666@gmail.com>
2020-09-06 16:53:24 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,19 @@
 from math import log2
 def ham(x:list,PlaceHolder=0): #size of 2^n-n-1
-    new=[PlaceHolder]
-    index=0
+    new=[PlaceHolder];index=0
     for i in range(1,len(x)+int.bit_length(len(x))+1):
         if log2(i).is_integer():new+=[PlaceHolder]
         else:new+=[x[index]];index+=1
     return new
 def encode(x:list):
     for i in range(int.bit_length(len(x))-1):
-        z=[]
-        for j in range(2**i):z+=x[2**i+j::2**(i+1)]
-        if sum(z)%2==1:x[2**i]=1-x[2**i]
+        if sum([sum(x[2**i+j::2**(i+1)]) for j in range(2**i)])%2==1:x[2**i]=1-x[2**i]
     if sum(x)%2==1:x[0]=1-x[0]
     return x
 def decode(x:list):
     index=0
     for i in range(int.bit_length(len(x))-1):
-        z=[]
-        for j in range(2**i):z+=x[2**i+j::2**(i+1)]
-        if sum(z)%2==1:index+=2**i
+        if sum([sum(x[2**i+j::2**(i+1)]) for j in range(2**i)])%2==1:index+=2**i
     if sum(x)%2==1:x[index]=1-x[index]
     return x
 PreHammedList=[1,0,1,0]
